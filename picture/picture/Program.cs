2d93fd41aa9948e0287518d97ea8bcb1ea2b4771
BUKI <buyunov2005@gmail.com>
2024-11-20 23:06:28 +0900

    Версия 1

---
+++ picture/picture/Program.cs
@@ -0,0 +1,18 @@
+﻿using pictureLib;
+while (true)
+{
+    Console.WriteLine("Введите имя художника: ");
+    string user = Console.ReadLine();
+    Picture us = new(user);
+    us.Print();
+
+    us.Time();
+    Console.Write("выйти из приложения, да или нет : ");
+    string esc = Console.ReadLine();
+
+    if (esc.ToLower() == "да")
+    {
+        break;
+    }
+}
+
---
+++ picture/pictureLib/Picture.cs
@@ -0,0 +1,41 @@
+﻿namespace pictureLib
+{
+    public class Picture
+    {
+        string name;
+        public Picture(string name)
+        {
+            this.name = name;
+        }
+        public void Print() => Console.WriteLine($"Имя художника: {name}");
+
+        public void Time()
+        {
+                Console.WriteLine($"Сколько время дано:");
+                double time = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Сколько частей картины всего:");
+                double Max = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Сколько частей картины нарисовано:");
+                double Min = Convert.ToDouble(Console.ReadLine());
+            if (Min == 0)
+            {
+                Console.WriteLine($"Сколько времи осталось: {time}");
+            }
+            else if (Min > Max || Max == 0)
+            {
+                Console.WriteLine($"Ошибка");
+                
+            }
+            else if (Min == Max)
+            {
+                Console.WriteLine($"Работа успешна завершена");
+            }
+            else
+            {
+                double a = (time / Max) * Min;
+                Console.WriteLine($"Сколько времи осталось: {a}");
+            }
+        }
+
+    }
+}
